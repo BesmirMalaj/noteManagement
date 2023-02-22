63efd7efc1b66b1074d7dc1b9376697ac5f08d8f
Besmir Malaj <besmirmalaj@dotbalkans.onmicrosoft.com>
2023-02-22 11:09:19 +0100

    the code is correct now

--- src/main/java/com/api/noteManagement/Note.java
+++ src/main/java/com/api/noteManagement/Note.java
@@ -9,14 +9,39 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class Note {
 
     private @Id @GeneratedValue Long id;
 
     private String title;
     private String content;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Note(Long id, String title, String content) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.content = content;
+	}
+	public Note() {
+		super();
+	}
 
 }
